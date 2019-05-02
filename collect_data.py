25e4489eaa35fe9cee08c5a1189ed88e984653e8
j0hnk1m <kim.john228@gmail.com>
2019-05-02 14:39:15 -0700

    worked on main.py

--- collect_data.py
+++ collect_data.py
@@ -3,34 +3,44 @@ import requests
 import re
 
 def imdb_feature_film(year):
-	# ex: https://www.imdb.com/year/2018/
+	"""
+	Given a specific year (from 2000~2018), returns a numpy array of 250 movies and their respective IMDB IDs.
+	Example link where this function scrapes data from: https://www.imdb.com/year/2018/
+	"""
+	print(year)
 	response = requests.get("https://www.imdb.com/year/" + str(year))
 	html = response.text
 	# totalCount = int(re.findall(r'<span>1-50 of ([^%]+?) titles.</span>[\r\n]+', html)[0].replace(',', ''))
 
-	movies = np.zeros((50, 2))
+	movies = np.zeros((0, 2))
 	for i in range(0, 5):  # 5 pages of 50 movies each = 250 top movies
-		print(i)
-		movies = np.concatenate((movies, np.array(re.findall(r'<a href="/title/([^:?%]+?)/"[\r\n]+> <img alt="([^%]+?)"[\r\n]+', html))))
+		movies = np.concatenate([movies, np.array(re.findall(r'<a href="/title/([^:?%]+?)/"[\r\n]+> <img alt="([^%]+?)"[\r\n]+', html))])
 		nextLink = "https://www.imdb.com" + re.findall(r'<a href="(/search/title\?title_type=feature&year=(?:.*)&start=(?:.*))"[\r\n]+class="lister-page-next next-page"', html)[0]
 		response = requests.get(nextLink)
 		html = response.text
 
-	movies=movies[50:]
 	return movies
 
 
 def wikipedia_in_film(year):
-	# ex: https://en.wikipedia.org/wiki/2018_in_film
+	"""
+	Given a specific year (from 2000~2018), returns a numpy array of movies.
+	Example link where this function scrapes data from: https://en.wikipedia.org/wiki/2018_in_film
+	"""
+	print(year)
 	response = requests.get("https://en.wikipedia.org/wiki/" + str(year) + "_in_film")
 	html = response.text
 	movies = np.unique(re.findall(r'<td><i><a href="/wiki/(?:.*)" title="(.*)">', html))
-	movies = np.array([i.replace('&#39;', "'").replace('&amp;', '&') for i in movies])
+	movies = np.array([i.replace('&#39;', "'").replace('&amp;', '&').replace(' (film)', '') for i in movies])
 	return movies
 
 
 def wildaboutmovies(year):
-	# ex: https://www.wildaboutmovies.com/2018_movies/
+	"""
+	Given a specific year (from 2000~2018), returns a numpy array of movies.
+	Example link where this function scrapes data from: https://www.wildaboutmovies.com/2018_movies/
+	"""
+	print(year)
 	response = requests.get("https://www.wildaboutmovies.com/" + str(year) + "_movies/")
 	html = response.text
 	movies = np.array(re.findall(r'<a href="/(?:.*)" alt="(.*)" /><p>', html))
--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import pandas as pd
+import numpy as np
+import collect_data
 
 def load_data():
-	df = pd.read_csv('bigml_data.csv')
+	df = pd.read_csv('./data/bigml_data.csv')
 	df.sort_values(['year', 'movie'], axis=0, ascending=True, inplace=True)
-	
+	return df
+
+def web_scrape():
+	imdb = np.zeros((0, 2))
+	wiki = np.zeros((0,))
+	wild = np.zeros((0,))
+	for y in list(range(2000, 2019)):
+		imdb = np.concatenate([imdb, collect_data.imdb_feature_film(y)])
+		wiki = np.concatenate([wiki, collect_data.wikipedia_in_film(y)])
+		wild = np.concatenate([wild, collect_data.wildaboutmovies(y)])
+	np.save('./saves/imdb', imdb)
+	np.save('./saves/wiki', wiki)
+	np.save('./saves/wild', wild)
+
+def main():
+	load_data()
+
+if __name__ == '__main__':
+	main()
