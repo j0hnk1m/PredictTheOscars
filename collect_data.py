c5a66eda1288283b19487c7a0b3ccb9aba1e32a2
j0hnk1m <kim.john228@gmail.com>
2019-05-02 00:04:11 -0700

    finished web scraping methods

--- collect_data.py
+++ collect_data.py
@@ -7,18 +7,17 @@ def imdb_feature_film(year):
 	# ex: https://www.imdb.com/year/2018/
 	response = requests.get("https://www.imdb.com/year/" + str(year))
 	html = response.text
-	totalCount = int(re.findall(r'<span>1-50 of ([^%]+?) titles.</span>[\r\n]+', html)[0].replace(',', ''))
+	# totalCount = int(re.findall(r'<span>1-50 of ([^%]+?) titles.</span>[\r\n]+', html)[0].replace(',', ''))
 
-	movies = np.array([])
-	for i in range(0, int(totalCount / 50)):
+	movies = np.zeros((50, 2))
+	for i in range(0, 5):  # 5 pages of 50 movies each = 250 top movies
 		print(i)
-		movies.append(re.findall(r'<a href="/title/([^:?%]+?)/"[\r\n]+> <img alt="([^%]+?)"[\r\n]+', html))
+		movies = np.concatenate((movies, np.array(re.findall(r'<a href="/title/([^:?%]+?)/"[\r\n]+> <img alt="([^%]+?)"[\r\n]+', html))))
 		nextLink = "https://www.imdb.com" + re.findall(r'<a href="(/search/title\?title_type=feature&year=(?:.*)&start=(?:.*))"[\r\n]+class="lister-page-next next-page"', html)[0]
 		response = requests.get(nextLink)
 		html = response.text
-	movies=movies
 
-
+	movies=movies[50:]
 	return movies
 
 
@@ -26,8 +25,8 @@ def wikipedia_in_film(year):
 	# ex: https://en.wikipedia.org/wiki/2018_in_film
 	response = requests.get("https://en.wikipedia.org/wiki/" + str(year) + "_in_film")
 	html = response.text
-	asdf = '<td><i><a href="/wiki/Insidious:_The_Last_Key" title="Insidious: The Last Key">'
-	movies = re.findall(r'<a href="/title/([^:%]+?)/"\\n>(?:\s)<img(?:\s)alt="([^%]+?)"\\nclass="loadlate', asdf)
+	movies = np.unique(re.findall(r'<td><i><a href="/wiki/(?:.*)" title="(.*)">', html))
+	movies = np.array([i.replace('&#39;', "'").replace('&amp;', '&') for i in movies])
 	return movies
 
 
@@ -35,6 +34,5 @@ def wildaboutmovies(year):
 	# ex: https://www.wildaboutmovies.com/2018_movies/
 	response = requests.get("https://www.wildaboutmovies.com/" + str(year) + "_movies/")
 	html = response.text
-	asdf = '">\n\n\n<a href="/title/tt4154756/"\n> <img alt="Avengers: Infinity War"\nclass="loadlate"\nloadlate='
-	movies = re.findall(r'<a href="/title/([^:%]+?)/"\\n>(?:\s)<img(?:\s)alt="([^%]+?)"\\nclass="loadlate', asdf)
+	movies = np.array(re.findall(r'<a href="/(?:.*)" alt="(.*)" /><p>', html))
 	return movies
