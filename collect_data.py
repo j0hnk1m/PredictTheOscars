786a6450f072921766978607638ab298413720a7
j0hnk1m <kim.john228@gmail.com>
2019-05-04 21:51:25 -0700

    finished tags

--- collect_data.py
+++ collect_data.py
@@ -23,10 +23,7 @@ def imdb_feature_film(year):
 	return df
 
 def collect_movie_info(id):
-	html = requests.get("https://www.imdb.com/title/" + str(id)).text
-
-	tags = re.findall('"genre": \[([^~]+)\],\\n(?:\s)"contentRating": "(.*)"(?:.* )', html)
-
+	# ---------------TAGS---------------
 	# certificate
 	# duration
 	# genre
@@ -41,105 +38,40 @@ def collect_movie_info(id):
 	# popularity
 	# awards_wins
 	# awards_nominations
-	#
-	# Oscar_Best_Picture_won
-	# Oscar_Best_Picture_nominated
-	# Oscar_Best_Director_won
-	# Oscar_Best_Director_nominated
-	# Oscar_Best_Actor_won
-	# Oscar_Best_Actor_nominated
-	# Oscar_Best_Actress_won
-	# Oscar_Best_Actress_nominated
-	# Oscar_Best_Supporting_Actor_won
-	# Oscar_Best_Supporting_Actor_nominated
-	# Oscar_Best_Supporting_Actress_won
-	# Oscar_Best_Supporting_Actress_nominated
-	# Oscar_Best_AdaScreen_won
-	# Oscar_Best_AdaScreen_nominated
-	# Oscar_Best_OriScreen_won
-	# Oscar_Best_OriScreen_nominated
-	# Oscar_nominated
-	# Oscar_nominated_categories
-	#
-	# Golden_Globes_won
-	# Golden_Globes_won_categories
-	# Golden_Globes_nominated
-	# Golden_Globes_nominated_categories
-	# BAFTA_won
-	# BAFTA_won_categories
-	# BAFTA_nominated
-	# BAFTA_nominated_categories
-	# Screen_Actors_Guild_won
-	# Screen_Actors_Guild_won_categories
-	# Screen_Actors_Guild_nominated
-	# Screen_Actors_Guild_nominated_categories
-	# Critics_Choice_won
-	# Critics_Choice_won_categories
-	# Critics_Choice_nominated
-	# Critics_Choice_nominated_categories
-	# Directors_Guild_won
-	# Directors_Guild_won_categories
-	# Directors_Guild_nominated
-	# Directors_Guild_nominated_categories
-	# Producers_Guild_won	Producers_Guild_won_categories
-	# Producers_Guild_nominated
-	# Producers_Guild_nominated_categories
-	# Art_Directors_Guild_won
-	# Art_Directors_Guild_won_categories
-	# Art_Directors_Guild_nominated
-	# Art_Directors_Guild_nominated_categories
-	# Writers_Guild_won
-	# Writers_Guild_won_categories
-	# Writers_Guild_nominated
-	# Writers_Guild_nominated_categories
-	# Costume_Designers_Guild_won
-	# Costume_Designers_Guild_won_categories
-	# Costume_Designers_Guild_nominated
-	# Costume_Designers_Guild_nominated_categories
-	# Online_Film_Television_Association_won
-	# Online_Film_Television_Association_won_categories
-	# Online_Film_Television_Association_nominated
-	# Online_Film_Television_Association_nominated_categories
-	# Online_Film_Critics_Society_won
-	# Online_Film_Critics_Society_won_categories
-	# Online_Film_Critics_Society_nominated
-	# Online_Film_Critics_Society_nominated_categories
-	# People_Choice_won
-	# People_Choice_won_categories
-	# People_Choice_nominated
-	# People_Choice_nominated_categories
-	# London_Critics_Circle_Film_won
-	# London_Critics_Circle_Film_won_categories
-	# London_Critics_Circle_Film_nominated
-	# London_Critics_Circle_Film_nominated_categories
-	# American_Cinema_Editors_won
-	# American_Cinema_Editors_won_categories
-	# American_Cinema_Editors_nominated
-	# American_Cinema_Editors_nominated_categories
-	# Hollywood_Film_won
-	# Hollywood_Film_won_categories
-	# Hollywood_Film_nominated
-	# Hollywood_Film_nominated_categories
-	# Austin_Film_Critics_Association_won
-	# Austin_Film_Critics_Association_won_categories
-	# Austin_Film_Critics_Association_nominated
-	# Austin_Film_Critics_Association_nominated_categories
-	# Denver_Film_Critics_Society_won
-	# Denver_Film_Critics_Society_won_categories
-	# Denver_Film_Critics_Society_nominated
-	# Denver_Film_Critics_Society_nominated_categories
-	# Boston_Society_of_Film_Critics_won
-	# Boston_Society_of_Film_Critics_won_categories
-	# Boston_Society_of_Film_Critics_nominated
-	# Boston_Society_of_Film_Critics_nominated_categories
-	# New_York_Film_Critics_Circle_won
-	# New_York_Film_Critics_Circle_won_categories
-	# New_York_Film_Critics_Circle_nominated
-	# New_York_Film_Critics_Circle_nominated_categories
-	# Los_Angeles_Film_Critics_Association_won
-	# Los_Angeles_Film_Critics_Association_won_categories
-	# Los_Angeles_Film_Critics_Association_nominated
-	# Los_Angeles_Film_Critics_Association_nominated_categories
-	# release_date.month
-	# release_date.day-of-month
+	html = requests.get("https://www.imdb.com/title/" + str(id)).text
+	tags = re.findall('"genre": \[([\s\S]+)\],\\n(?:\s*)"contentRating": "(.*)",\\n[\s\S]+<strong title="(.*) based on '
+					  '([,0-9]+) user ratings">[\s\S]+<span itemprop="reviewCount">([,0-9]+) user</span>[\s\S]+<span itemprop'
+					  '="reviewCount">([,0-9]+) critic</span>[\s\S]+<time datetime="PT(\d+)M">\\n[\s\S]+<div class="summary_text">\\n'
+					  '(.*)\\n[\s\S]+<div class="metacriticScore score_favorable titleReviewBarSubItem">\\n<span>(\d{2,3})<'
+					  '[\s\S]+ ([,0-9]+)\\n[\s\S]+\(<span class="titleOverviewSprite popularity[\s\S]+<span class='
+					  '"awards-blurb">[\s\S]+Another[\s\S]+ (\d+) wins &amp; (\d+) nominations.[\s\S]+<h4 class="inline"'
+					  '>Gross USA:</h4> \$([,0-9]+), <span', html)[0]
+	tags = [" ".join(i.split()).replace('"', '') for i in tags]
+	order = [1, 6, 0, 2, 8, 7, 3, 12, 4, 5, 9, 10, 11]
+	tags = [tags[i] for i in order]
+	return tags
+
+
+	# ---------------AWARDS---------------
+	# Oscar
+	# Golden Globe
+	# BAFTA
+	# Screen Actors Guild
+	# Critics Choice
+	# Directors Guild
+	# Producers Guild
+	# Art Directors Guild
+	# Writers Guild
+	# Costume Designers Guild
+	# Online Film Television Association
+	# Online Film Critics Society
+	# People Choice
+	# London Critics Circle Film
+	# American Cinema Editors
+	# Hollywood Film
+	# Austin Film Critics Association
+	# Denver Film Critics_Society
+	# Boston Society of Film Critics
+	# New York Film Critics Circle
+	# Los Angeles Film Critics Association
 
