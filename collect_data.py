e2f44f43654bba3619edbae56154ec422b22be05
j0hnk1m <kim.john228@gmail.com>
2019-05-04 17:51:16 -0700

    organized data

--- collect_data.py
+++ collect_data.py
@@ -10,47 +10,136 @@ def imdb_feature_film(year):
 	Example link where this function scrapes data from: https://www.imdb.com/year/2018/
 	"""
 	print(year)
-	response = requests.get("https://www.imdb.com/year/" + str(year))
-	html = response.text
+	html = requests.get("https://www.imdb.com/year/" + str(year)).text
 
 	movies = np.zeros((0, 2))
 	for i in range(0, 7):  # 7 pages of 50 movies each = 500 top movies
 		movies = np.concatenate([movies, np.flip(np.array(re.findall(r'<a href="/title/([^:?%]+?)/"[\r\n]+> <img alt="([^%]+?)"[\r\n]+', html)))])
 		nextLink = "https://www.imdb.com" + re.findall(r'<a href="(/search/title\?title_type=feature&year=(?:.*)&start=(?:.*))"[\r\n]+class="lister-page-next next-page"', html)[0]
-		response = requests.get(nextLink)
-		html = response.text
+		html = requests.get(nextLink).text
 
-	df = pd.DataFrame(movies, columns=['movie', 'imdb_id'])
+	df = pd.DataFrame(movies, columns=['movie', 'movie_id'])
 	df.insert(0, 'year', [year]*movies.shape[0], True)
 	return df
 
-
-def wikipedia_in_film(year):
-	"""
-	Given a specific year (from 2000~2018), returns a numpy array of movies.
-	Example link where this function scrapes data from: https://en.wikipedia.org/wiki/2018_in_film
-	"""
-	print(year)
-	response = requests.get("https://en.wikipedia.org/wiki/" + str(year) + "_in_film")
-	html = response.text
-	movies = np.unique(re.findall(r'<td><i><a href="/wiki/(?:.*)" title="(.*)">', html))
-	movies = np.array([i.replace('&#39;', "'").replace('&amp;', '&').replace(' (film)', '') for i in movies])
-
-	df = pd.DataFrame(movies, columns=['movie'])
-	df.insert(0, 'year', [year] * movies.shape[0], True)
-	return df
-
-
 def collect_movie_info(id):
-	response = requests.get("https://www.imdb.com/title/" + str(id))
-	html = response.text
+	html = requests.get("https://www.imdb.com/title/" + str(id)).text
 
-	certificate = re.findall(r'"contentRating": "PG-13"', html)
-	Duration
-	genre
-	IMDB rating
-	Synopsis
-	Votes
-	Gross
-	Metacritic
-	Release date
+	tags = re.findall('"genre": \[([^~]+)\],\\n(?:\s)"contentRating": "(.*)"(?:.* )', html)
+
+	# certificate
+	# duration
+	# genre
+	# rate
+	# metascore
+	# synopsis
+	# votes
+	# gross
+	# release_date
+	# user_reviews
+	# critic_reviews
+	# popularity
+	# awards_wins
+	# awards_nominations
+	#
+	# Oscar_Best_Picture_won
+	# Oscar_Best_Picture_nominated
+	# Oscar_Best_Director_won
+	# Oscar_Best_Director_nominated
+	# Oscar_Best_Actor_won
+	# Oscar_Best_Actor_nominated
+	# Oscar_Best_Actress_won
+	# Oscar_Best_Actress_nominated
+	# Oscar_Best_Supporting_Actor_won
+	# Oscar_Best_Supporting_Actor_nominated
+	# Oscar_Best_Supporting_Actress_won
+	# Oscar_Best_Supporting_Actress_nominated
+	# Oscar_Best_AdaScreen_won
+	# Oscar_Best_AdaScreen_nominated
+	# Oscar_Best_OriScreen_won
+	# Oscar_Best_OriScreen_nominated
+	# Oscar_nominated
+	# Oscar_nominated_categories
+	#
+	# Golden_Globes_won
+	# Golden_Globes_won_categories
+	# Golden_Globes_nominated
+	# Golden_Globes_nominated_categories
+	# BAFTA_won
+	# BAFTA_won_categories
+	# BAFTA_nominated
+	# BAFTA_nominated_categories
+	# Screen_Actors_Guild_won
+	# Screen_Actors_Guild_won_categories
+	# Screen_Actors_Guild_nominated
+	# Screen_Actors_Guild_nominated_categories
+	# Critics_Choice_won
+	# Critics_Choice_won_categories
+	# Critics_Choice_nominated
+	# Critics_Choice_nominated_categories
+	# Directors_Guild_won
+	# Directors_Guild_won_categories
+	# Directors_Guild_nominated
+	# Directors_Guild_nominated_categories
+	# Producers_Guild_won	Producers_Guild_won_categories
+	# Producers_Guild_nominated
+	# Producers_Guild_nominated_categories
+	# Art_Directors_Guild_won
+	# Art_Directors_Guild_won_categories
+	# Art_Directors_Guild_nominated
+	# Art_Directors_Guild_nominated_categories
+	# Writers_Guild_won
+	# Writers_Guild_won_categories
+	# Writers_Guild_nominated
+	# Writers_Guild_nominated_categories
+	# Costume_Designers_Guild_won
+	# Costume_Designers_Guild_won_categories
+	# Costume_Designers_Guild_nominated
+	# Costume_Designers_Guild_nominated_categories
+	# Online_Film_Television_Association_won
+	# Online_Film_Television_Association_won_categories
+	# Online_Film_Television_Association_nominated
+	# Online_Film_Television_Association_nominated_categories
+	# Online_Film_Critics_Society_won
+	# Online_Film_Critics_Society_won_categories
+	# Online_Film_Critics_Society_nominated
+	# Online_Film_Critics_Society_nominated_categories
+	# People_Choice_won
+	# People_Choice_won_categories
+	# People_Choice_nominated
+	# People_Choice_nominated_categories
+	# London_Critics_Circle_Film_won
+	# London_Critics_Circle_Film_won_categories
+	# London_Critics_Circle_Film_nominated
+	# London_Critics_Circle_Film_nominated_categories
+	# American_Cinema_Editors_won
+	# American_Cinema_Editors_won_categories
+	# American_Cinema_Editors_nominated
+	# American_Cinema_Editors_nominated_categories
+	# Hollywood_Film_won
+	# Hollywood_Film_won_categories
+	# Hollywood_Film_nominated
+	# Hollywood_Film_nominated_categories
+	# Austin_Film_Critics_Association_won
+	# Austin_Film_Critics_Association_won_categories
+	# Austin_Film_Critics_Association_nominated
+	# Austin_Film_Critics_Association_nominated_categories
+	# Denver_Film_Critics_Society_won
+	# Denver_Film_Critics_Society_won_categories
+	# Denver_Film_Critics_Society_nominated
+	# Denver_Film_Critics_Society_nominated_categories
+	# Boston_Society_of_Film_Critics_won
+	# Boston_Society_of_Film_Critics_won_categories
+	# Boston_Society_of_Film_Critics_nominated
+	# Boston_Society_of_Film_Critics_nominated_categories
+	# New_York_Film_Critics_Circle_won
+	# New_York_Film_Critics_Circle_won_categories
+	# New_York_Film_Critics_Circle_nominated
+	# New_York_Film_Critics_Circle_nominated_categories
+	# Los_Angeles_Film_Critics_Association_won
+	# Los_Angeles_Film_Critics_Association_won_categories
+	# Los_Angeles_Film_Critics_Association_nominated
+	# Los_Angeles_Film_Critics_Association_nominated_categories
+	# release_date.month
+	# release_date.day-of-month
+
--- main.py
+++ main.py
@@ -2,39 +2,33 @@ import pandas as pd
 import numpy as np
 import collect_data
 import os
-import math
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 
 from keras.models import Sequential
 from keras.layers import Dense
-np.random.seed(7)
+np.random.seed(1)
 
 
 def retrieve_movie_list():
-	if not os.path.exists('./data/imdb.csv') or not os.path.exists('./data/wiki.csv'):
+	if not os.path.exists('./data/imdb.csv'):
 		imdb = collect_data.imdb_feature_film(2000)
-		wiki = collect_data.wikipedia_in_film(2000)
 		for y in list(range(2001, 2019)):
 			imdb = imdb.append(collect_data.imdb_feature_film(y))
-			wiki = wiki.append(collect_data.wikipedia_in_film(y))
 
 		# Removes duplicate movies
+		df = pd.read_csv('./data/winners_nominees.csv')
 		temp = []
-		for index, row in wiki.iterrows():
-			if row['movie'] not in list(imdb['movie']):
-				temp.append([row['movie'], row['year']])
-		wiki = pd.DataFrame(temp, columns=['movie', 'year'])
+		for index, row in imdb.iterrows():
+			if row['movie'] not in list(df['movie']):
+				temp.append([row['year'], row['movie'], row['movie_id']])
+		imdb = pd.DataFrame(temp, columns=['year', 'movie', 'movie_id'])
 
 		imdb.to_csv('./data/imdb.csv')
-		wiki.to_csv('./data/wiki.csv')
 	else:
 		imdb = pd.read_csv('./data/imdb.csv')
-		wiki = pd.read_csv('./data/wiki.csv')
 
-	combined = pd.concat([imdb, wiki],ignore_index=True, sort=True)
-	combined.to_csv('./data/combined.csv')
-	return combined
+	return imdb
 
 
 def build_model(type):
@@ -48,22 +42,13 @@ def build_model(type):
 
 
 def main():
-	df = pd.read_csv('./data/bigml_data.csv')
+	df = pd.read_csv('./data/winners_nominees.csv')
 	df.sort_values(['year', 'movie'], axis=0, ascending=True, inplace=True)
-	combined = retrieve_movie_list()
+	imdb = retrieve_movie_list()
 
-	# # See what movies from the oscar winners/nominees bigml data are missing in the web-scraped data
-	# missing=[]
-	# for m in list(df['movie']):
-	# 	if m not in list(combined['movie']):
-	# 		missing.append(m)
+	for index, row in imdb.iterrows():
+		movie_id = row['movie_id']
 
-	for index, row in combined.iterrows():
-		imdb_id = row['imdb_id']
-		if type(imdb_id) is str:
-
-		else:
-			print("no imdb")
 
 if __name__ == '__main__':
 	main()
